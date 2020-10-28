5d006f969b0c573119cf80ecb5b3d053ef770e64
Justin Corbin <jcorbin2290@gmail.com>
2020-10-28 12:49:44 -0600

    🚧 Added initial Route to Welcome page and initial render success

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,29 @@
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-          Start Build
-        </p>
-    </div>
-  );
+import './App.css';
+import Welcome from '../WelcomePage/Welcome'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      right: 0,
+      wrong: 0,
+      questions: null
+    }
+  }
+
+  render() {
+    return (
+      <div className= "App" >
+        <Route exact path='/'>
+          <Welcome />
+        </Route>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Components/WelcomePage/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className='welcome-container'>
+      <p className='title'>Pop Quiz</p>
+      <button className='start-btn'>Start</button>
+    </div>
+  )
+}
+
+export default Welcome;
