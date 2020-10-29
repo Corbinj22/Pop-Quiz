c390b6e8f332224f5293e9b8017aad9813ddc11b
Justin Corbin <jcorbin2290@gmail.com>
2020-10-29 15:43:09 -0600

    🔮 Mocked API request, spoofed data, and setState

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { fetchedData } from '../../apiRequestFake'
 
 import './App.css';
 import Welcome from '../WelcomePage/Welcome';
@@ -14,6 +15,27 @@ class App extends Component {
       wrong: 0,
       questions: null
     }
+  }
+
+  componentDidMount = async () => {
+    await this.setState({
+      questions: fetchedData
+    })
+    await this.spoofData()
+  }
+
+  spoofData = () => {
+    let spoofedData = this.state.questions.map(question => {
+      const randomNum = Number((Math.random() * 3))
+      const setNum = Number(randomNum.toFixed())
+      const copiedData = [...question.incorrect]
+      question = {...question, questionBank : copiedData}
+      question.questionBank.splice(setNum, 0, question.correct)
+      return question
+    })
+    this.setState({
+      questions: spoofedData
+    })
   }
 
   render() {
