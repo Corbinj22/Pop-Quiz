409da8f8426679a615856eeb72bc31d557d274dd
Justin Corbin <jcorbin2290@gmail.com>
2020-10-31 19:28:15 -0600

    🔮 changes include adding props for QuestionBox

--- src/Components/GameView/GameView.js
+++ src/Components/GameView/GameView.js
@@ -3,11 +3,11 @@ import ScoreBox from '../ScoreBox/ScoreBox';
 import QuestionBox from '../QuestionBox/QuestionBox'
 import './GameView.css';
 
-const GameView = ({ gameState, checkAnswer, submitAnswer, cycleQuestion }) => {
+const GameView = ({ gameState, checkAnswer, submitAnswer, cycleQuestion, resetBeenSubmitted }) => {
   return (
     <div className='GameView-container'>
       <ScoreBox scores={gameState}/>
-      <QuestionBox checkAnswer={checkAnswer} questions={gameState.questions} cycleQuestion={cycleQuestion} submitAnswer={submitAnswer} beenSubmitted={gameState.beenSubmitted}/>
+      <QuestionBox checkAnswer={checkAnswer} questions={gameState.questions} cycleQuestion={cycleQuestion} resetBeenSubmitted={resetBeenSubmitted} submitAnswer={submitAnswer} beenSubmitted={gameState.beenSubmitted}/>
     </div>
   )
 }
