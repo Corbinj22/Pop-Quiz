1008bf3ca93ac30cf05948c98a5818cc8aa227de
Justin Corbin <jcorbin2290@gmail.com>
2020-11-01 13:39:21 -0700

    📊 unit testing for QuestionBox and start of intergration test for App

--- src/Components/GameView/GameView.test.js
+++ src/Components/GameView/GameView.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GameView from './GameView';
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,22 +12,7 @@ let fakeScores = {
     questions: null,
     beenSubmitted: false
   }
-
-  let fakeData = [
-    {
-      "question": "What was Tandem previous name?",
-      "incorrect": ["Tandem", "Burger Shack", "Extraordinary Humans"],
-      "correct": "Devmynd",
-      "questionBank": ["Tandem", "Devmynd", "Burger Shack", "Extraordinary Humans"]
-    },
-    {
-      "question": "In Shakespeare's play Julius Caesar, Caesar's last words were...",
-      "incorrect": ["Iacta alea est!", "Vidi, vini, vici", "Aegri somnia vana"],
-      "correct": "Et tu, Brute?",
-      "questionBank": ["Et tu, Brute?", "Iacta alea est!", "Vidi, vini, vici", "Aegri somnia vana"]
-    }
-  ]
-
+  
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<BrowserRouter> <GameView gameState={fakeScores}/> </BrowserRouter>, div);
@@ -36,7 +21,7 @@ let fakeScores = {
 
   describe('<GameView />', () => {
     it('should display user game info', () => {
-      const { getByPlaceholderText, getByLabelText, debug } = render(<BrowserRouter> <GameView gameState={fakeScores}/> </BrowserRouter>);
+      const { queryByLabelText } = render(<BrowserRouter> <GameView gameState={fakeScores}/> </BrowserRouter>);
       const title = screen.queryByLabelText('title')
     })
   })
